4964abf69bcb8d43d28927a8b037116c080965ad
tmzjf12 <tmzjf12@naver.com>
2025-06-04 14:11:36 +0900

    스텝1 화면구성

--- Assets/Scripts/ATMManager.cs
+++ Assets/Scripts/ATMManager.cs
@@ -1,18 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class NewBehaviourScript : MonoBehaviour
+public class ATMManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public TextMeshProUGUI balanceText;
+    public TextMeshProUGUI cashText;
+    public Button depositButton;
+    public Button withdrawButton;
+
+    public int balance = 50000;
+    public int cash = 100000;
+
+    void UpdateUI()
+    {
+        balanceText.text = string.Format("{0:N0}", balance);
+        cashText.text = string.Format("{0:N0}", cash);
+    }
+    
+    
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        UpdateUI();
     }
 }
